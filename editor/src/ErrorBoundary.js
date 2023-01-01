eef7cc8e0ab88973a830b1c79e9a9e9d927c2834
Ryan McAlister <rymc26@gmail.com>
2023-01-01 16:24:25 -0700

    removed unused vars

--- editor/src/ErrorBoundary.js
+++ editor/src/ErrorBoundary.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 
-class ErrorBoundary extends React.Component {
+class ErrorBoundary extends Component {
     constructor(props) {
       super(props);
       this.state = {hasError: false};
